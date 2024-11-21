8ab7e94131c2b095fcf306c948174d5c65d7b326
kmao <kmao@core.infinex.xyz>
2024-11-21 12:02:42 +1100

    Update page.tsx

--- src/app/tas/[slug]/page.tsx
+++ src/app/tas/[slug]/page.tsx
@@ -10,33 +10,33 @@ type Params = {
   slug: string;
 };
 
-const schema = keystatic.collections.trfs.schema;
+const schema = keystatic.collections.tas.schema;
 const fields = Object.keys(schema);
 
 export async function generateStaticParams(): Promise<Params[]> {
-  const topics = await reader.collections.trfs.all();
-  return topics.map((trf) => ({
-    slug: trf.slug,
+  const topics = await reader.collections.tas.all();
+  return topics.map((ta) => ({
+    slug: ta.slug,
   }));
 }
 
 export default async function Page({ params }: { params: Params }) {
-  const trf = await reader.collections.trfs.read(params.slug, {
+  const ta = await reader.collections.tas.read(params.slug, {
     resolveLinkedFiles: true,
   });
 
-  if (!trf) return notFound();
+  if (!ta) return notFound();
 
   return (
     <PageContainer>
-      <Header currentPage="TRFs" />
+      <Header currentPage="TAs" />
       <div className="my-8 flex border-t border-slate-800">
-        <Back href="/trfs">Back to all TRFs</Back>
+        <Back href="/tas">Back to all TAs</Back>
       </div>
-      <ID status={trf.status}>TRF-{number(trf.id!)}</ID>
-      <PageTitle>{trf.title}</PageTitle>
-      <Properties fields={fields} data={trf} />
-      <Renderer node={trf.content.node} />
+      <ID status={ta.status}>ta-{number(ta.id!)}</ID>
+      <PageTitle>{ta.title}</PageTitle>
+      <Properties fields={fields} data={ta} />
+      <Renderer node={ta.content.node} />
     </PageContainer>
   );
 }
